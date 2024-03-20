2cb2ebdd926d64ea2d1eec554754c81ea7a8a873
F11snipe <f11snipe@gmail.com>
2024-03-20 17:34:04 -0600

    working yaml config and lib functions

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 use std::fs::File;
 use std::io::BufReader;
 use serde::{Serialize, Deserialize};
-use std::collections::HashMap;
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Point {
-    x: f64,
-    y: f64,
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct ConfigData {
-    fs: HashMap<String, Vec<String>>,
-    ps: HashMap<String, Vec<String>>,
-}
+use rust_examples::{self, watch};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct ConfigGroup {
@@ -22,7 +10,7 @@ struct ConfigGroup {
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct ConfigDataTest {
+struct ConfigData {
     fs: ConfigGroup,
     ps: ConfigGroup,
 }
@@ -31,55 +19,43 @@ fn main() -> Result<(), serde_yaml::Error> {
     let file = File::open("conf/config.yaml").expect("Missing config.yaml file");
     let buf_reader = BufReader::new(file);
 
-    let testing: ConfigDataTest = serde_yaml::from_reader(buf_reader)?;
+    let testing: ConfigData = serde_yaml::from_reader(buf_reader)?;
 
     dbg!(&testing);
 
-    match testing.fs.watch {
-        Some(lst) => println!("FS Watch: {:?}", lst),
-        None => println!("FS Watch: NOTHING"),
+    let fs_watch = &testing.fs.watch.unwrap_or_else(|| Vec::new());
+    let fs_blacklist = &testing.fs.blacklist.unwrap_or_else(|| Vec::new());
+
+    let ps_watch = &testing.ps.watch.unwrap_or_else(|| Vec::new());
+    let ps_blacklist = &testing.ps.blacklist.unwrap_or_else(|| Vec::new());
+
+    for f in fs_watch {
+        println!("FS Watch: {}", f);
+    }
+    for f in fs_blacklist {
+        println!("FS Blacklist: {}", f);
     }
 
-    // match testing.fs.blacklist {
-    //     Some(lst) => println!("FS Blacklist: {:?}", lst),
-    //     None => println!("FS Blacklist: NOTHING"),
-    // }
-
-    match testing.ps.watch {
-        Some(lst) => println!("PS Watch: {:?}", lst),
-        None => println!("PS Watch: NOTHING"),
+    for p in ps_watch {
+        println!("PS Watch: {}", p);
+    }
+    for p in ps_blacklist {
+        println!("PS Blacklist: {}", p);
     }
 
-    match testing.ps.blacklist {
-        Some(lst) => println!("PS Watch: {:?}", lst),
-        None => println!("PS Blacklist: NOTHING"),
+    for d in fs_watch {
+        if let Err(e) = rust_examples::walk(d, fs_blacklist) {
+            println!("error: {:?}", e);
+        }
     }
 
-    let fsbl = &testing.fs.blacklist.unwrap_or_else(|| Vec::new());
-
-    for f in fsbl {
-        println!("FS BL: {}", f);
+    if let Err(e) = rust_examples::watch(".", fs_blacklist) {
+        println!("error: {:?}", e);
     }
 
-    for f in fsbl {
-        println!("FS BL: {}", f);
+    if let Err(e) = watch(".", fs_blacklist) {
+        println!("error: {:?}", e);
     }
 
-// borrow of partially moved value: `testing.fs.watch`
-// partial move occurs because value has type `Vec<std::string::String>`, which does not implement the `Copy` trait
-    // if let Some(lst) = &testing.fs.watch {
-    //     println!("FS watch enabled!");
-    //     for f in lst {
-    //         println!("FS WATCH: {}", f);
-    //     }
-    // }
-
-    let point = Point { x: 1.0, y: 2.0 };
-
-    let yaml = serde_yaml::to_string(&point)?;
-    assert_eq!(yaml, "x: 1.0\ny: 2.0\n");
-
-    let deserialized_point: Point = serde_yaml::from_str(&yaml)?;
-    assert_eq!(point, deserialized_point);
     Ok(())
 }
